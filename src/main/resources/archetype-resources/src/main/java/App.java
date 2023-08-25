ea0f2ba14d31a32de2c40b937ca71bad77d6c323
PST\basribaki <basribaki@commercedc.com.my>
2023-08-25 16:49:58 +0800

    adding dockerfile

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is for jenkins testing" );
+        System.out.println( "This is for jenkins second round" );
     }
 }
