1b4b4844779a3c7014824a24a755cdf861e12429
PST\basribaki <basribaki@commercedc.com.my>
2023-08-25 15:32:10 +0800

    first test file

---
+++ src/main/resources/archetype-resources/src/main/java/user.java
@@ -0,0 +1,32 @@
+
+
+public class user{
+
+    public String name;
+    public String age:
+    public String jobDesc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getJobDesc() {
+        return jobDesc;
+    }
+
+    public void setJobDesc(String jobDesc) {
+        this.jobDesc = jobDesc;
+    }
+}
